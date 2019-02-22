5257cc2b8116d004b07573cc1a4204d7c974edd8
pndemo <ndemopaul1@gmail.com>
2019-02-22 23:26:43 +0300

    Update RSpec config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,6 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
+require 'database_cleaner'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
@@ -68,10 +69,15 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
+  # FactoryBot
+  config.include FactoryBot::Syntax::Methods
+
+  # Trancating tables for transactions
   config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
+    DatabaseCleaner.strategy = :transaction
   end
+
   config.around(:each) do |example|
     DatabaseCleaner.cleaning do
       example.run
