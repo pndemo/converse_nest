eb239b00c8db005f4cd22b0ea342587e048f912d
pndemo <ndemopaul1@gmail.com>
2019-02-22 23:47:43 +0300

    Test application models

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should belong_to(:post) }
+
+  it { should validate_presence_of(:content) }
+  it { should validate_presence_of(:created_by) }
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many(:comments).dependent(:destroy) }
+
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:content) }
+  it { should validate_presence_of(:created_by) }
 end
